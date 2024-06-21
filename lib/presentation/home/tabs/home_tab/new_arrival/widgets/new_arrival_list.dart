7282c5d545bbe5a067fe6a672aba9bae84d58fe1
Noureen saad <noureensaad086@gmail.com>
2024-06-21 22:18:31 +0300

    new arrival (list + grid) design done

---
+++ lib/presentation/home/tabs/home_tab/new_arrival/widgets/new_arrival_list.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:slash_task/core/di/di.dart';
+import 'package:slash_task/core/reusable_component/product_widget.dart';
+import '../view_model/new_arrival_cubit.dart';
+
+class NewArrivalList extends StatelessWidget {
+  const NewArrivalList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text("New Arrival",style: Theme.of(context).textTheme.headlineLarge,),
+        SizedBox(height: 10,),
+        BlocProvider(
+          create: (context)=>getIt<NewArrivalCubit>()..getNewArrival(),
+          child: BlocBuilder<NewArrivalCubit,NewArrivalState>(
+            builder: (context,state){
+              if(state is NewArrivalSuccess){
+                return Expanded(
+                    child: ListView.separated(
+                        scrollDirection: Axis.horizontal,
+                        itemBuilder: (context,index){
+                          return ProductWidget(price: state.newArrival[index].price,name: state.newArrival[index].name,imagePath: state.newArrival[index].image,);
+                        },
+                        separatorBuilder: (context,index)=>SizedBox(width: 5,),
+                        itemCount: state.newArrival.length
+                    )
+                );
+              }
+              else{
+                return Center(child: CircularProgressIndicator.adaptive(),);
+              }
+            },
+          ),
+        )
+      ],
+    );
+  }
+}
